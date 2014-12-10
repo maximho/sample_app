93bfe8cb8d51cf1385031fc75823d5a2bbc411d9
Maximilianoho <maximho20@gmail.com>
2014-12-10 16:42:47 +0100

    Routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  #get '/help' => 'static_pages#help'
+  get '/help' => 'static_pages#help'
 
   get 'static_pages/help'# => 'static_pages#help'
   get 'static_pages/about'
